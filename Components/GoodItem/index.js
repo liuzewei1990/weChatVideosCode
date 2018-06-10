3d109797650d2bac39497f8751695cdb6b734464
冰河 <543347844@qq.com>
2018-06-10 23:46:25 +0800

    增加cell组件，待完善。

---
+++ Components/GoodItem/index.js
@@ -0,0 +1,25 @@
+// Components/GoodItem/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imgUrl: String,
+    title: String,
+    num: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
